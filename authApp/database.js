208a10ad3a69b8284c534edca27ddd236c6a316e
Shivamya94 <yadavshivam90440@gmail.com>
2024-12-20 22:44:38 +0530

    .

--- authApp/database.js
+++ authApp/database.js
@@ -3,8 +3,9 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 exports.connect = () => {
-    mongoose.connect(process.env.MONGODB_URL, {}
-    )
+    mongoose.connect(process.env.MONGODB_URL, {
+        
+    })
     .then( () => {console.log("DB connected Successfully")} )
     .catch ( (err) => {
         console.log("DB Connection issued");
