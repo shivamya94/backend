2e2c9c7f2dcdd4562d9f672c8fb57be9c7661127
Shivamya94 <yadavshivam90440@gmail.com>
2024-12-21 14:39:01 +0530

    j

--- authApp/index.js
+++ authApp/index.js
@@ -6,5 +6,16 @@ const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
 
-require("./config");
+require("./config/database").connect();
+
+//route import and mount
+const user = require("./routes/user");
+app.use("api/v1",user);
+
+//activities
+
+app.listen(PORT,() => {
+    console.log(`App is listening at ${PORT}`);
+    
+})
 
