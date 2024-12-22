bc6d04f6b2cc0f614be41817deef56e6daa5c2f3
Shivamya94 <yadavshivam90440@gmail.com>
2024-12-23 05:09:14 +0530

    i

--- authApp/controllers/Auth.js
+++ authApp/controllers/Auth.js
@@ -1,6 +1,7 @@
 const bcrypt = require("bcrypt");
 const User = require("../models/User");
 const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 //signup route handler
 exports.signup = async (req,res) => {
@@ -73,9 +74,33 @@ exports.login = async (req,res) => {
             });
         }
 
+        const payload = {
+            email:user.email,
+            id:user._id,
+            role:user.role,
+        }
+
         //verify password & generate a JWT token
         if(await bcrypt.compare(password,user.password)){
+            //password match
+            let token = jwt.sign(payload,
+                                process.env.JWT_SECRET,
+                                {
+                                    expiresIn:"2h",
+                                });
+            user.token = token;
+            user.password = undefined;
+            const options = {
+                expires: new Date( Date.now() + 3 * 24 * 60 * 60 * 1000),
+                httpOnly:true,
 
+            }
+            res.cookie("token", token, options).status(200).json({
+                success:true,
+                token,
+                user,
+                message:'User Logged in successfully',
+            });
         }
         else{
             //password do not match
@@ -87,6 +112,11 @@ exports.login = async (req,res) => {
 
     }
     catch(error){
+        console.log(error);
+        return res.status(500).json({
+            success:false,
+            message:"failure",
+        });
 
     }
 }
