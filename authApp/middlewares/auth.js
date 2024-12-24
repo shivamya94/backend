70b5b12c3092c110a93c0355a043fa058cd35585
Shivamya94 <yadavshivam90440@gmail.com>
2024-12-24 20:56:31 +0530

    g

--- authApp/middlewares/auth.js
+++ authApp/middlewares/auth.js
@@ -17,10 +17,10 @@ exports.auth = (req,res,next) => {
 
         //verify the token
         try{
-            const decode = jwt.verify(token,process.env.JWT_SECRET);
-            console.log(decode);
+            const payload = jwt.verify(token,process.env.JWT_SECRET);
+            console.log(payload);
             //why this?
-            req.user = decode;
+            req.user = payload;
         } catch(error) {
             return res.status(401).json({
                 success:false,
@@ -62,10 +62,10 @@ exports.isStudent = (req,res,next) => {
 
 exports.isAdmin = (req,res,next) => {
     try{
-        if(req,user.role !=="Student") {
+        if(req,user.role !=="Admin") {
             return res.status(401).json({
                 success:false,
-                message:"This is a protected route for students",
+                message:"This is a protected route for Admin",
             });
         }
         next();
