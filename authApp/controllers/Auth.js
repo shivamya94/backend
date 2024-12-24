bb47aec53e9577d779f14f03af587891f6da24cd
Shivamya94 <yadavshivam90440@gmail.com>
2024-12-24 15:51:15 +0530

    k

--- authApp/controllers/Auth.js
+++ authApp/controllers/Auth.js
@@ -88,14 +88,16 @@ exports.login = async (req,res) => {
                                 {
                                     expiresIn:"2h",
                                 });
+            // user = user.toObject();
             user.token = token;
             user.password = undefined;
+            
             const options = {
                 expires: new Date( Date.now() + 3 * 24 * 60 * 60 * 1000),
                 httpOnly:true,
 
             }
-            res.cookie("token", token, options).status(200).json({
+            res.cookie("Shivamtoken", token, options).status(200).json({
                 success:true,
                 token,
                 user,
